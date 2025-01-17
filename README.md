74db3f832d0c98e289e643aa0df78ea89917817d
Dmitrii Losunov <dmitrij.dmitriy32@yandex.ru>
2025-01-16 23:49:16 -0800

    Исправлен тест test_personal_account_order_history

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Diplom_3
+# Stellar Burgers Test Automation
+
+# Описание
+Автотесты для веб-приложения **Stellar Burgers**, покрывающие ключевой функционал:
+- Лента заказов.
+- Оформление заказов.
+- Проверка разделов "История заказов" и "В работе".
+
+# Запуск тестов
+Установите зависимости:
+   ```bash
+   pip install -r requirements.txt
+
+Запустите тесты в Chrome:
+pytest tests --browser=chrome -v
+
+Для Firefox:
+pytest tests --browser=firefox -v
--- tests/test_personal_account.py
+++ tests/test_personal_account.py
@@ -1,9 +1,11 @@
 import allure
-
+import time
 from pages.main_page import MainPage
 from pages.personal_account_page import PersonalAccountPage
 from pages.constructor_page import ConstructorPage
 from config import USER_EMAIL, USER_PASSWORD
+from locators.main_page_locators import MainPageLocators
+from locators.constructor_locators import ConstructorLocators
 
 @allure.feature("Личный кабинет")
 class TestPersonalAccount:
@@ -16,10 +18,17 @@ class TestPersonalAccount:
         personal_account_page.enter_email(USER_EMAIL)
         personal_account_page.enter_password(USER_PASSWORD)
         personal_account_page.click_login_button()
+
         constructor_page = ConstructorPage(driver)
-        main_page.drag_and_drop_element()
+        bun = main_page.find_element_with_wait(MainPageLocators.BUNS_TAB)
+        basket = main_page.find_element_with_wait(MainPageLocators.BASKET)
+        constructor_page.drag_and_drop_element(bun, basket)
+        time.sleep(2)
         constructor_page.click_order_button()
-        constructor_page.close_order_modal()
+        time.sleep(2)
+        constructor_page.click_to_element(ConstructorLocators.CLOSE_ORDER_MODAL_BUTTON)
+        time.sleep(2)
+
         personal_account_page.go_to_personal_account()
         personal_account_page.go_to_order_history()
         assert personal_account_page.is_order_present_in_history()
